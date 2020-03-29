99c9c6f9560c9298b4f8b691a0527b34a3de8135
rachit2501 <rac.sri25@gmail.com>
2020-03-29 23:48:39 +0530

    production

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = next({
 
 const routes = require("./routes");
 const handler = routes.getRequestHandler(app);
-
+const port = process.env.PORT || 3000;
 app.prepare().then(() => {
-  createServer(handler).listen(3000, err => {
+  createServer(handler).listen(port, err => {
     if (err) throw err;
-    console.log("Ready on localhost:3000");
+    console.log(`Ready on localhost:${port}`);
   });
   ``;
 });
