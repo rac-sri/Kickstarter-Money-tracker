100f2b5cec3aa3b2e00c7a31c787a241e316ce4e
rachit2501 <rac.sri25@gmail.com>
2020-03-29 18:49:44 +0530

    deployment bugs

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,13 +1,11 @@
-
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
 const compiledFactory = require("./build/CampaignFactory.json");
 
 const provider = new HDWalletProvider(
-  "bird carpet pudding trick vacant rough fatal debate enact problem thrive enhance",
+  "call glow acoustic vintage front ring trade assist shuffle mimic volume reject",
   "https://rinkeby.infura.io/v3/3dc8b2e3489c4260904f45a4e74a56dc"
 );
-
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -15,13 +13,12 @@ const deploy = async () => {
 
   console.log("Attempting to deploy from account", accounts[0]);
 
-  let contract = new web3.eth.Contract(JSON.parse(compiledFactory.interface));
-
-  const result = await contract
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
     .deploy({ data: "0x" + compiledFactory.bytecode })
-    .send({ gas: 3000000, from: accounts[0] });
-
-  console.log("contract deployed to : " + result.options.address);
+    .send({ gas: "4000000", from: accounts[0] });
+  console.log(result);
+  console.log("Contract deployed to", result.options.address);
 };
-
 deploy();
--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,10 +1,9 @@
-import web3 from './web3';
-import CampaignFactory from './build/CampaignFactory.json';
+import web3 from "./web3";
+import CampaignFactory from "./build/CampaignFactory.json";
 
-
-const instance =  new web3.eth.Contract(
-    JSON.parse(CampaignFactory.interface),
-    '0x5A76bf45785bBa3ADF023d71cff06dDeB0F0e4bc'
-)
+const instance = new web3.eth.Contract(
+  JSON.parse(CampaignFactory.interface),
+  "0xdc04977a2078c8ffdf086d618d1f961b6c546222"
+);
 
 export default instance;
